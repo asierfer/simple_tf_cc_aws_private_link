423034e1667eb834114df156c4516379d5979b2b
Asier Fernández <asierfer@gmail.com>
2023-08-10 15:44:35 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,413 @@
+terraform {
+  required_version = ">= 0.14.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "= 2.32.0"
+    }
+    confluent = {
+      source  = "confluentinc/confluent"
+      version = "1.51.0"
+    }
+  }
+}
+
+provider "confluent" {
+  cloud_api_key    = var.confluent_cloud_api_key
+  cloud_api_secret = var.confluent_cloud_api_secret
+}
+
+resource "confluent_environment" "staging" {
+  display_name = "${var.prefix}-Staging"
+}
+
+# Stream Governance and Kafka clusters can be in different regions as well as different cloud providers,
+# but you should to place both in the same cloud and region to restrict the fault isolation boundary.
+data "confluent_schema_registry_region" "essentials" {
+  cloud   = "AWS"
+  region  = "${sr_region}"
+  package = "ESSENTIALS"
+}
+
+resource "confluent_schema_registry_cluster" "essentials" {
+  package = data.confluent_schema_registry_region.essentials.package
+
+  environment {
+    id = confluent_environment.staging.id
+  }
+
+  region {
+    # See https://docs.confluent.io/cloud/current/stream-governance/packages.html#stream-governance-regions
+    id = data.confluent_schema_registry_region.essentials.id
+  }
+}
+
+resource "confluent_network" "private-link" {
+  display_name     = "${var.prefix}-Private Link Network"
+  cloud            = "AWS"
+  region           = var.region
+  connection_types = ["PRIVATELINK"]
+  zones            = keys(var.subnets_to_privatelink)
+  environment {
+    id = confluent_environment.staging.id
+  }
+}
+
+resource "confluent_private_link_access" "aws" {
+  display_name = "${var.prefix}-AWS Private Link Access"
+  aws {
+    account = var.aws_account_id
+  }
+  environment {
+    id = confluent_environment.staging.id
+  }
+  network {
+    id = confluent_network.private-link.id
+  }
+}
+
+resource "confluent_kafka_cluster" "dedicated" {
+  display_name = "${var.prefix}-inventory"
+  availability = "MULTI_ZONE"
+  cloud        = confluent_network.private-link.cloud
+  region       = confluent_network.private-link.region
+  dedicated {
+    cku = 2
+  }
+  environment {
+    id = confluent_environment.staging.id
+  }
+  network {
+    id = confluent_network.private-link.id
+  }
+}
+
+// 'app-manager' service account is required in this configuration to create 'orders' topic and grant ACLs
+// to 'app-producer' and 'app-consumer' service accounts.
+resource "confluent_service_account" "app-manager" {
+  display_name = "${var.prefix}-app-manager"
+  description  = "Service account to manage 'inventory' Kafka cluster"
+}
+
+resource "confluent_role_binding" "app-manager-kafka-cluster-admin" {
+  principal   = "User:${confluent_service_account.app-manager.id}"
+  role_name   = "CloudClusterAdmin"
+  crn_pattern = confluent_kafka_cluster.dedicated.rbac_crn
+}
+
+resource "confluent_api_key" "app-manager-kafka-api-key" {
+  display_name = "${var.prefix}-app-manager-kafka-api-key"
+  description  = "Kafka API Key that is owned by 'app-manager' service account"
+
+  # Set optional `disable_wait_for_ready` attribute (defaults to `false`) to `true` if the machine where Terraform is not run within a private network
+  disable_wait_for_ready = true
+
+  owner {
+    id          = confluent_service_account.app-manager.id
+    api_version = confluent_service_account.app-manager.api_version
+    kind        = confluent_service_account.app-manager.kind
+  }
+
+  managed_resource {
+    id          = confluent_kafka_cluster.dedicated.id
+    api_version = confluent_kafka_cluster.dedicated.api_version
+    kind        = confluent_kafka_cluster.dedicated.kind
+
+    environment {
+      id = confluent_environment.staging.id
+    }
+  }
+
+  # The goal is to ensure that
+  # 1. confluent_role_binding.app-manager-kafka-cluster-admin is created before
+  # confluent_api_key.app-manager-kafka-api-key is used to create instances of
+  # confluent_kafka_topic, confluent_kafka_acl resources.
+  # 2. Kafka connectivity through AWS PrivateLink is setup.
+  depends_on = [
+    confluent_role_binding.app-manager-kafka-cluster-admin,
+
+    confluent_private_link_access.aws,
+    aws_vpc_endpoint.privatelink,
+    aws_route53_record.privatelink,
+    aws_route53_record.privatelink-zonal,
+  ]
+}
+/*
+// Provisioning Kafka Topics requires access to the REST endpoint on the Kafka cluster
+// If Terraform is not run from within the private network, this will not work
+resource "confluent_kafka_topic" "orders" {
+  kafka_cluster {
+    id = confluent_kafka_cluster.dedicated.id
+  }
+  topic_name    = "orders"
+  rest_endpoint = confluent_kafka_cluster.dedicated.rest_endpoint
+  credentials {
+    key    = confluent_api_key.app-manager-kafka-api-key.id
+    secret = confluent_api_key.app-manager-kafka-api-key.secret
+  }
+}
+
+resource "confluent_service_account" "app-consumer" {
+  display_name = "app-consumer"
+  description  = "Service account to consume from 'orders' topic of 'inventory' Kafka cluster"
+}
+
+resource "confluent_api_key" "app-consumer-kafka-api-key" {
+  display_name = "app-consumer-kafka-api-key"
+  description  = "Kafka API Key that is owned by 'app-consumer' service account"
+
+  # Set optional `disable_wait_for_ready` attribute (defaults to `false`) to `true` if the machine where Terraform is not run within a private network
+  # disable_wait_for_ready = true
+
+  owner {
+    id          = confluent_service_account.app-consumer.id
+    api_version = confluent_service_account.app-consumer.api_version
+    kind        = confluent_service_account.app-consumer.kind
+  }
+
+  managed_resource {
+    id          = confluent_kafka_cluster.dedicated.id
+    api_version = confluent_kafka_cluster.dedicated.api_version
+    kind        = confluent_kafka_cluster.dedicated.kind
+
+    environment {
+      id = confluent_environment.staging.id
+    }
+  }
+
+  # The goal is to ensure that Kafka connectivity through AWS PrivateLink is setup.
+  depends_on = [
+    confluent_private_link_access.aws,
+    aws_vpc_endpoint.privatelink,
+    aws_route53_record.privatelink,
+    aws_route53_record.privatelink-zonal,
+  ]
+}
+
+// Provisioning Kafka ACLs requires access to the REST endpoint on the Kafka cluster
+// If Terraform is not run from within the private network, this will not work
+resource "confluent_kafka_acl" "app-producer-write-on-topic" {
+  kafka_cluster {
+    id = confluent_kafka_cluster.dedicated.id
+  }
+  resource_type = "TOPIC"
+  resource_name = confluent_kafka_topic.orders.topic_name
+  pattern_type  = "LITERAL"
+  principal     = "User:${confluent_service_account.app-producer.id}"
+  host          = "*"
+  operation     = "WRITE"
+  permission    = "ALLOW"
+  rest_endpoint = confluent_kafka_cluster.dedicated.rest_endpoint
+  credentials {
+    key    = confluent_api_key.app-manager-kafka-api-key.id
+    secret = confluent_api_key.app-manager-kafka-api-key.secret
+  }
+}
+
+resource "confluent_service_account" "app-producer" {
+  display_name = "app-producer"
+  description  = "Service account to produce to 'orders' topic of 'inventory' Kafka cluster"
+}
+
+resource "confluent_api_key" "app-producer-kafka-api-key" {
+  display_name = "app-producer-kafka-api-key"
+  description  = "Kafka API Key that is owned by 'app-producer' service account"
+
+  # Set optional `disable_wait_for_ready` attribute (defaults to `false`) to `true` if the machine where Terraform is not run within a private network
+  # disable_wait_for_ready = true
+
+  owner {
+    id          = confluent_service_account.app-producer.id
+    api_version = confluent_service_account.app-producer.api_version
+    kind        = confluent_service_account.app-producer.kind
+  }
+
+  managed_resource {
+    id          = confluent_kafka_cluster.dedicated.id
+    api_version = confluent_kafka_cluster.dedicated.api_version
+    kind        = confluent_kafka_cluster.dedicated.kind
+
+    environment {
+      id = confluent_environment.staging.id
+    }
+  }
+
+  # The goal is to ensure that Kafka connectivity through AWS PrivateLink is setup.
+  depends_on = [
+    confluent_private_link_access.aws,
+    aws_vpc_endpoint.privatelink,
+    aws_route53_record.privatelink,
+    aws_route53_record.privatelink-zonal,
+  ]
+}
+
+// Note that in order to consume from a topic, the principal of the consumer ('app-consumer' service account)
+// needs to be authorized to perform 'READ' operation on both Topic and Group resources:
+// confluent_kafka_acl.app-consumer-read-on-topic, confluent_kafka_acl.app-consumer-read-on-group.
+// https://docs.confluent.io/platform/current/kafka/authorization.html#using-acls
+
+// Provisioning Kafka ACLs requires access to the REST endpoint on the Kafka cluster
+// If Terraform is not run from within the private network, this will not work
+resource "confluent_kafka_acl" "app-consumer-read-on-topic" {
+  kafka_cluster {
+    id = confluent_kafka_cluster.dedicated.id
+  }
+  resource_type = "TOPIC"
+  resource_name = confluent_kafka_topic.orders.topic_name
+  pattern_type  = "LITERAL"
+  principal     = "User:${confluent_service_account.app-consumer.id}"
+  host          = "*"
+  operation     = "READ"
+  permission    = "ALLOW"
+  rest_endpoint = confluent_kafka_cluster.dedicated.rest_endpoint
+  credentials {
+    key    = confluent_api_key.app-manager-kafka-api-key.id
+    secret = confluent_api_key.app-manager-kafka-api-key.secret
+  }
+}
+
+// Provisioning Kafka ACLs requires access to the REST endpoint on the Kafka cluster
+// If Terraform is not run from within the private network, this will not work
+resource "confluent_kafka_acl" "app-consumer-read-on-group" {
+  kafka_cluster {
+    id = confluent_kafka_cluster.dedicated.id
+  }
+  resource_type = "GROUP"
+  // The existing values of resource_name, pattern_type attributes are set up to match Confluent CLI's default consumer group ID ("confluent_cli_consumer_<uuid>").
+  // https://docs.confluent.io/confluent-cli/current/command-reference/kafka/topic/confluent_kafka_topic_consume.html
+  // Update the values of resource_name, pattern_type attributes to match your target consumer group ID.
+  // https://docs.confluent.io/platform/current/kafka/authorization.html#prefixed-acls
+  resource_name = "confluent_cli_consumer_"
+  pattern_type  = "PREFIXED"
+  principal     = "User:${confluent_service_account.app-consumer.id}"
+  host          = "*"
+  operation     = "READ"
+  permission    = "ALLOW"
+  rest_endpoint = confluent_kafka_cluster.dedicated.rest_endpoint
+  credentials {
+    key    = confluent_api_key.app-manager-kafka-api-key.id
+    secret = confluent_api_key.app-manager-kafka-api-key.secret
+  }
+}
+*/
+# https://docs.confluent.io/cloud/current/networking/private-links/aws-privatelink.html
+# Set up the VPC Endpoint for AWS PrivateLink in your AWS account
+# Set up DNS records to use AWS VPC endpoints
+provider "aws" {
+  region = var.region
+}
+
+locals {
+  hosted_zone = length(regexall(".glb", confluent_kafka_cluster.dedicated.bootstrap_endpoint)) > 0 ? replace(regex("^[^.]+-([0-9a-zA-Z]+[.].*):[0-9]+$", confluent_kafka_cluster.dedicated.bootstrap_endpoint)[0], "glb.", "") : regex("[.]([0-9a-zA-Z]+[.].*):[0-9]+$", confluent_kafka_cluster.dedicated.bootstrap_endpoint)[0]
+}
+
+data "aws_vpc" "privatelink" {
+  id = var.vpc_id
+}
+
+data "aws_availability_zone" "privatelink" {
+  for_each = var.subnets_to_privatelink
+  zone_id  = each.key
+}
+
+locals {
+  bootstrap_prefix = split(".", confluent_kafka_cluster.dedicated.bootstrap_endpoint)[0]
+}
+
+resource "aws_security_group" "privatelink" {
+  # Ensure that SG is unique, so that this module can be used multiple times within a single VPC
+  name        = "${var.prefix}-ccloud-privatelink_${local.bootstrap_prefix}_${var.vpc_id}"
+  description = "Confluent Cloud Private Link minimal security group for ${confluent_kafka_cluster.dedicated.bootstrap_endpoint} in ${var.vpc_id}"
+  vpc_id      = data.aws_vpc.privatelink.id
+
+  ingress {
+    # only necessary if redirect support from http/https is desired
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = [data.aws_vpc.privatelink.cidr_block]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = [data.aws_vpc.privatelink.cidr_block]
+  }
+
+  ingress {
+    from_port   = 9092
+    to_port     = 9092
+    protocol    = "tcp"
+    cidr_blocks = [data.aws_vpc.privatelink.cidr_block]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_vpc_endpoint" "privatelink" {
+  vpc_id            = data.aws_vpc.privatelink.id
+  service_name      = confluent_network.private-link.aws[0].private_link_endpoint_service
+  vpc_endpoint_type = "Interface"
+
+  security_group_ids = [
+    aws_security_group.privatelink.id,
+  ]
+
+  subnet_ids          = [for zone, subnet_id in var.subnets_to_privatelink : subnet_id]
+  private_dns_enabled = false
+
+  depends_on = [
+    confluent_private_link_access.aws,
+  ]
+}
+
+//Amazon Route 53 is a scalable and highly available Domain Name System service
+
+resource "aws_route53_zone" "privatelink" {
+  name = local.hosted_zone
+
+  vpc {
+    vpc_id = data.aws_vpc.privatelink.id
+  }
+}
+
+resource "aws_route53_record" "privatelink" {
+  count   = length(var.subnets_to_privatelink) == 1 ? 0 : 1
+  zone_id = aws_route53_zone.privatelink.zone_id
+  name    = "*.${aws_route53_zone.privatelink.name}"
+  type    = "CNAME"
+  ttl     = "60"
+  records = [
+    aws_vpc_endpoint.privatelink.dns_entry[0]["dns_name"]
+  ]
+}
+
+locals {
+  endpoint_prefix = split(".", aws_vpc_endpoint.privatelink.dns_entry[0]["dns_name"])[0]
+}
+
+resource "aws_route53_record" "privatelink-zonal" {
+  for_each = var.subnets_to_privatelink
+
+  zone_id = aws_route53_zone.privatelink.zone_id
+  name    = length(var.subnets_to_privatelink) == 1 ? "*" : "*.${each.key}"
+  type    = "CNAME"
+  ttl     = "60"
+  records = [
+    format("%s-%s%s",
+      local.endpoint_prefix,
+      data.aws_availability_zone.privatelink[each.key].name,
+      replace(aws_vpc_endpoint.privatelink.dns_entry[0]["dns_name"], local.endpoint_prefix, "")
+    )
+  ]
+}
